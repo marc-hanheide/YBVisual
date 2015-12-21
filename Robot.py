bec8990876d5ab318000ca1b9d3c6acef21cc162
webbam46 <webbam46@aol.com>
2015-12-21 20:12:10 +0000

    move_base planning

--- Robot.py
+++ Robot.py
@@ -4,10 +4,86 @@ import rospy
 import json
 import moveit_commander
 import moveit_msgs.msg
+import actionlib
 import geometry_msgs.msg
 from geometry_msgs import *
 import move_base_msgs.msg
+from move_base_msgs.msg import *
+from actionlib_msgs.msg import *
 from std_msgs.msg import *
+
+
+#
+# Robotbase is used to send goals to the base
+#
+class RobotBase:
+    #Initialise
+    def __init__(self):
+        rospy.loginfo("Creating robot base");       
+        #Publisher used to manually control the robot
+        self.cmd_vel_publisher = rospy.Publisher('/move_base/cmd_vel',geometry_msgs.msg.Twist);
+        #Subscribe to the move_base action server
+        self.move_base_server = actionlib.SimpleActionClient('move_base',MoveBaseAction);
+        #move base goal publisher
+        #self.move_base_goal_publisher = rospy.Publisher('/move_base/goal',move_base_msgs.msg.MoveBaseActionGoal);                
+        rospy.loginfo("Starting move_base action server..");
+        #Wait unil the acton server is available
+        self.move_base_server.wait_for_server(rospy.Duration(60));
+        rospy.loginfo("Started move_base action server");
+        #Created!    
+        rospy.loginfo("Created robot base");
+    #Stop moving the base - cancel all goals
+    def Stop(self):
+        self.Move(0,0,0,0);
+        self.move_base_server.cancel_goal();
+    #Create a goal message
+    def CreateGoal(self,x,y,z,w):
+        #Create msg object
+        g = MoveBaseGoal()
+        g.target_pose.header.frame_id = "base_link";
+        g.target_pose.header.stamp = rospy.Time.now()
+        g.target_pose.pose.position.x = x; #Move in X axis by meters
+        g.target_pose.pose.position.y = y; #Move in Y axis by meters 
+        g.target_pose.pose.position.z = z; #Move in Z axis by meters
+        g.target_pose.pose.orientation.w = w; #We need to specify an orientation > 0
+        return g
+    #Move base in direction
+    def Move(self,lx,ly,lz,az):
+        #Create a twist message
+        twist = geometry_msgs.msg.Twist()
+        twist.linear.x = lx
+        twist.linear.y = ly
+        twist.linear.z = lz
+        twist.angular.x = 0
+        twist.angular.y = 0
+        twist.angular.z = az
+        #log event
+        rospy.loginfo("Driving Youbot")
+        
+        #publish
+        self.cmd_vel_publisher.publish(twist)
+             
+        
+        
+        #now publish
+        #fo i in range(30):
+           #self.cmd_vel_publisher.publish(twist)
+           #rospy.sleep(0.1) # 30*0.1 = 3.0
+    
+    #Move the base to a goal
+    def MoveTo(self,x,y,z):
+        #Create the goal 
+        goal = self.CreateGoal(x,y,z,1.0);
+        #Send the robot to the goal
+        rospy.loginfo("Moving robot towards goal");
+        self.move_base_server.send_goal(goal);
+        #get result
+        goalresult= self.move_base_server.wait_for_result(rospy.Duration(50));
+        #stop when reached
+        self.Stop();            
+    
+    
+
 
 
 #
@@ -29,10 +105,8 @@ class Youbot:
         self.group = moveit_commander.MoveGroupCommander("arm_1")
         #Display trajectory publisher
         self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',moveit_msgs.msg.DisplayTrajectory)
-        #move base cmd publisher
-        self.move_base_cmdvel_publisher = rospy.Publisher('/move_base/cmd_vel',geometry_msgs.msg.Twist);
-        #move base goal publisher
-        self.move_base_goal_publisher = rospy.Publisher('/move_base/goal',move_base_msgs.msg.MoveBaseActionGoal);
+        #robot base
+        self.base = RobotBase();       
         #Youbot has been initialised
         rospy.loginfo("Youbot initialised")      
         #Finally - print the initial state of the youbot        
@@ -43,24 +117,17 @@ class Youbot:
         print self.robot.get_current_state()
         print " "
     #Drive the robot
-    def Drive(self,lx,ly,lz,ax,ay,az):
+    def Drive(self,lx,ly,lz,az):
         #Create a twist message
-        twist = geometry_msgs.msg.Twist()
-        twist.linear.x = lx
-        twist.linear.y = ly
-        twist.linear.z = lz
-        twist.angular.x = ax
-        twist.angular.y = ay
-        twist.angular.z = az
-        #log event
-        rospy.loginfo("Driving Youbot")
-        #now publish
-        for i in range(30):
-           self.move_base_cmdvel_publisher.publish(twist)
-           rospy.sleep(0.1) # 30*0.1 = 3.0
+        self.base.Move(lx,ly,lz,az);
+    #Drive the robot to a goal
+    def DriveTo(self,x,y,z):
+        #Create appropriate ROS message
+        self.base.MoveTo(x,y,z);
     #Stop the robot
     def Stop(self):
-        self.Drive(0,0,0,0,0,0);
+        #stop the base
+        self.base.Stop();
 
 #
 # JSON Object
@@ -116,32 +183,39 @@ class RobotController:
             #Move type command
             if(_type == "MOVE"):
                     print "Move command";
+                    #Get amount value
+                    amount = float(_val);
+                    print "Amount: " + str(amount);
                     if(_att == "FORWARDS"):
                         print "Move forward command";
-                        self.robot.Drive(1,0,0,0,0,0);
+                        #self.robot.Drive(1,0,0,0,0,0);
+                        self.robot.DriveTo(amount,0,0);
                         #Process the data here - move the robot forward
                     if(_att == "BACK"):
                         print "Move backward command";
-                        self.robot.Drive(-1,0,0,0,0,0);
+                        #self.robot.Drive(-1,0,0,0,0,0);
+                        self.robot.DriveTo(-amount,0,0);
                         #Move the robot back
                     if(_att == "LEFT"):
                         print "Move left command";
-                        self.robot.Drive(0,1,0,0,0,0);
+                        #self.robot.Drive(0,1,0,0,0,0);
+                        self.robot.DriveTo(0,amount,0);
                         #Move the robot left
                     if(_att == "RIGHT"):
                         print "Move right command";
-                        self.robot.Drive(0,-1,0,0,0,0);
+                        #self.robot.Drive(0,-1,0,0,0,0);
+                        self.robot.DriveTo(0,-amount,0);
                         #Move the robot right
             #Rotate type command
             if(_type == "ROTATE"):
                     if(_att == "RIGHT"):
                         #Rotate the robot right
                         print "Rotate right command";
-                        self.robot.Drive(0,0,0,0,0,1);
+                        self.robot.Drive(0,0,0,1);
                     if(_att == "LEFT"):
                         #Rotate the robot left
                         print "Rotate left command";
-                        self.robot.Drive(0,0,0,0,0,-1);
+                        self.robot.Drive(0,0,0,-1);
             #halt type command
             if(_type == "HALT"):
                 print "Halt command";
